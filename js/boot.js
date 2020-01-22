9710d6b7b84ee1769a0aa1438af4865f834081c6
Andrea <andreagrimmiger1@hotmail.com>
2020-01-22 17:38:52 +0100

    Create boot.js

---
+++ js/boot.js
@@ -0,0 +1,25 @@
+var Puzzle = Puzzle || {};
+
+Puzzle.Boot = function(){};
+
+//setting game configuration and loading the assets for the loading screen
+Puzzle.Boot.prototype = {
+  preload: function() {
+  	this.game.load.bitmapFont('blue', 'assets/fonts/blue.png', 'assets/fonts/blue.fnt');
+  },
+  create: function() {
+    this.game.stage.backgroundColor = '#1d2224';
+
+    //scaling options
+	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	this.scale.minWidth = 240;
+	this.scale.minHeight = 170;
+	this.scale.maxWidth = 2880;
+	this.scale.maxHeight = 1920;
+	
+	//have the game centered horizontally
+	this.scale.pageAlignHorizontally = true;
+    
+    this.state.start('Preload');
+  }
+};
